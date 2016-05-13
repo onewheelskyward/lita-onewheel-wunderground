753be3b1a480f2620b1a1a99a5231da6887d70e3
Andrew Kreps <andrew.kreps@gmail.com>
2016-05-13 14:53:16 -0700

    Added !recordlow.

--- lib/lita/handlers/onewheel_wunderground.rb
+++ lib/lita/handlers/onewheel_wunderground.rb
@@ -6,18 +6,25 @@ module Lita
       config :api_key
 
       route /^recordhigh$/, :record_high, command: true
+      route /^recordlow$/, :record_low, command: true
 
       def record_high(response)
-        data = get_record_high
+        data = get_data
 
         response.reply data['almanac']['temp_high']['record']['F'] + "F in " + data['almanac']['temp_high']['recordyear']
       end
 
-      def get_record_high
+      def get_data
         uri = "http://api.wunderground.com/api/#{config.api_key}/almanac/q/OR/Portland.json"
         puts "getting #{uri}"
         data = RestClient.get uri
         JSON.parse data
+      end
+
+      def record_low(response)
+        data = get_data
+
+        response.reply data['almanac']['temp_low']['record']['F'] + "F in " + data['almanac']['temp_low']['recordyear']
       end
 
       Lita.register_handler(self)
--- spec/lita/handlers/onewheel_wunderground_spec.rb
+++ spec/lita/handlers/onewheel_wunderground_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Lita::Handlers::OnewheelWunderground, lita_handler: true do
   it { is_expected.to route_command('recordhigh') }
+  it { is_expected.to route_command('recordlow') }
 
   before do
     mock = File.open('spec/fixtures/Portland.json').read
@@ -11,5 +12,10 @@ describe Lita::Handlers::OnewheelWunderground, lita_handler: true do
   it 'gives a record high number' do
     send_command 'recordhigh'
     expect(replies.last).to eq('78F in 1983')
+  end
+
+  it 'gives a record low number' do
+    send_command 'recordlow'
+    expect(replies.last).to eq('31F in 1977')
   end
 end
